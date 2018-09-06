2065ba3904ef7886af049420bcf245299edfe17d
huangxiaoqin <huangxiaoqin@qudian.com>
2018-09-06 09:58:23 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+"""
+    @author:huang xiaoqin
+    @time: 2018/9/5
+    @desc:
+"""
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('datasets/Data.csv')
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values
+
+# Handling the missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+# compute mean
+imputer = imputer.fit(X[:, 1:3])
+# use mean to replace
+X[:,1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
+
+# which column is the feature needed one-hot
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+
+# the scaling is needed on the training set and test set
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
