f11056c5f69a246d44d404228b33ae205d05614d
huangxiaoqin <huangxiaoqin@qudian.com>
2018-10-25 11:34:39 +0800

    day8

---
+++ day5_8_LR.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+"""
+    @author:huang xiaoqin
+    @time: 2018/10/25
+    @desc:
+"""
+
+"""
+Logistic regression  gives us a discrete outcome while linear regression gives us a continuous outcome.
+That is to say: 逻辑回归（Logistic Regression）是用于处理因变量为分类变量的回归问题,也被称为广义线性回归模型。
+重要的是：预测函数和损失函数的形式，具体推导过程可见https://blog.csdn.net/chibangyuxun/article/details/53148005
+需要注意的是在直接使用python包时，要注意各种参数的设定。
+"""
